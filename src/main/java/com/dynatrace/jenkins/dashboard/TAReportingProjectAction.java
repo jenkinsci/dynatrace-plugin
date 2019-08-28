f61c3bf946deb208b6b8ac497ac3879cec6c5aa0
Dariusz.Glugla <dariusz.glugla@dynatrace.com>
2019-08-28 14:14:05 +0200

    solved few warnings added @RequirePOST to FormValidation method in TAGlobalConfiguration

--- src/main/java/com/dynatrace/jenkins/dashboard/TAReportingProjectAction.java
+++ src/main/java/com/dynatrace/jenkins/dashboard/TAReportingProjectAction.java
@@ -75,9 +75,9 @@ public class TAReportingProjectAction implements Action{
 	private static final Color COLOR_IMPROVED = new Color(0x00a6fb);
 	private static final Color COLOR_VOLATILE = new Color(0xffe11c);
 
-	private abstract class GraphImpl extends Graph {
+	private abstract static class GraphImpl extends Graph {
 
-		protected GraphImpl() {
+		GraphImpl() {
 			super(-1, 500, 300); // cannot use timestamp, since ranges may change
 		}
 
@@ -114,7 +114,7 @@ public class TAReportingProjectAction implements Action{
 			categoryaxis.setCategoryMargin(0.0D);
 			categoryaxis.setCategoryLabelPositions(CategoryLabelPositions.UP_90);
 
-			// KN: force the chart axis to have only integer values
+			// force the chart axis to have only integer values
 			plot.getRangeAxis().setStandardTickUnits(NumberAxis.createIntegerTickUnits());
 
 			return chart;
@@ -123,6 +123,7 @@ public class TAReportingProjectAction implements Action{
 
 	private final Job<?, ?> project;
 
+	@SuppressWarnings("WeakerAccess")
 	public TAReportingProjectAction(Job<?,?> project) {
 		this.project = project;
 	}
@@ -155,7 +156,7 @@ public class TAReportingProjectAction implements Action{
 		List<TAReport> reports = getExistingReportsList(MAX_BUILD_NUMBER_TO_SHOW_ON_CHART);
 		for (TAReport report : reports) {
 			Map<TestStatus, Integer> summary = report.getSummary();
-			summaries.put(new ChartUtil.NumberOnlyBuildLabel((Run<?, ?>) report.getBuild()), summary);
+			summaries.put(new ChartUtil.NumberOnlyBuildLabel(report.getBuild()), summary);
 		}
 
 		final Graph graph = new GraphImpl() {
@@ -200,7 +201,8 @@ public class TAReportingProjectAction implements Action{
 
 			if (buildAction != null) {
 				report = buildAction.getCurrentReport();
-			} else { // KN: FOR BACKWARD COMPATIBILITY
+			} else {
+				// backward compatibility
 				report = UtilsCompat.getCompatReport(currentBuild);
 			}
 
